d58a5b3d9f7b2b30968ab79dfae5941c43ad8fb0
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-09-18 18:45:29 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int logInfo(char *str) {
+    printf("\033[34m[info]\033[0m %s\n", str);
+
+    return 0;
+};
+
+int logError(char *str) {
+    printf("\033[31m[error]\033[0m %s\n", str);
+
+    return 1;
+};
+
+int main() {
+    char str[100];
+    char result[114];
+    printf("write your name: ");
+    scanf("%[^\n]%*c", str);
+
+    if (strlen(str) > 100) {
+        logError("name is too long");
+        return 1;
+    };
+
+    strcpy(result, "Hello, ");
+    strcat(result, str);
+    strcat(result, "!");
+    
+    logInfo(result);
+
+    return 0;
+};
