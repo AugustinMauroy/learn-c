36d8d40bb168b382bfeb15b29a6fac9e12baf0b3
Augustin Mauroy <augustin.mauroy@outlook.fr>
2023-09-18 19:01:11 +0200

    fix

--- play with string in terminal/main.c
+++ play with string in terminal/main.c
@@ -3,20 +3,18 @@
 
 int logInfo(char *str) {
     printf("\033[34m[info]\033[0m %s\n", str);
-
     return 0;
 };
 
 int logError(char *str) {
     printf("\033[31m[error]\033[0m %s\n", str);
-
     return 1;
 };
 
 int main() {
     char str[100];
     char result[114];
-    printf("write your name: ");
+    logInfo("write your name: ");
     scanf("%[^\n]%*c", str);
 
     if (strlen(str) > 100) {
