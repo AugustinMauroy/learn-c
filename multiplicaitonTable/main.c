41b395f7fe0d364bef3e217a876e1ce1f1b00f56
Augustin Mauroy <augustin.mauroy@outlook.fr>
2024-04-15 21:57:08 +0200

    update multiplication table

--- multiplicaitonTable/main.c
+++ multiplicaitonTable/main.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define GREEN "\033[0;32m"
+#define RESET "\033[0m"
 
 int main(void) {
     int userInput;
@@ -9,9 +13,31 @@ int main(void) {
         scanf("%d", &userInput);
     } while (userInput < 1 || userInput > 10);
 
+    printf("\033[1;1H\033[2J"); // Clear the screen
+    printf("╭──────────────────────╮\n");
+    printf("│ %sMultiplication Table%s │\n", GREEN, RESET);
+    printf("╞──────────────────────╡\n");
     for (int i = 1; i <= 10; i++) {
-        printf("%d * %d = %d\n", userInput, i, userInput * i);
+        char text[100];
+        sprintf(text, "%2d * %2d = %3d", userInput, i, userInput * i);
+
+        int padding = (22 - strlen(text)) / 2;
+        printf("│");
+        for (int j = 0; j < padding; j++) {
+            printf(" ");
+        }
+        printf("%s", text);
+        for (int j = 0; j < padding; j++) {
+            printf(" ");
+        }
+        // add extra space if the text is odd
+        if (strlen(text) % 2 != 0) {
+            printf(" ");
+        }
+        printf("│\n");
     }
+
+    printf("╰──────────────────────╯\n");
 
     return EXIT_SUCCESS;
 }
